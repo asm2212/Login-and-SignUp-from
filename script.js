bb1edc9aa774f2aa8cefa0168197873c9e8c2d79
asm2212 <asmareadmasu0@gmail.com>
2023-12-23 18:46:13 +0300

    some update and responsive

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ let login = document.querySelector('login');
 let create = document.querySelector('create');
 let container = document.querySelector('.container');
 
-login
+login.onclick = function(){
+    container.classList.add(signinFrom);
+};
+
+create.onclick = function(){
+    container.classList.remove(signinFrom);
+};
